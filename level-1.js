0d51e90c663fdb826354894ad7645906291907f0
Meepen <meepdarknessmeep@gmail.com>
2018-06-21 14:24:33 -0400

    restructure to set up multiple attacks

--- level-1.js
+++ level-1.js
@@ -1,44 +1,68 @@
 const GAME = gGame;
 const CLICKS_PER_SECOND = 10;
 
-const WORST_SCORE = -1 / 0;
-
-function ScoreEnemy(enemy) {
-    if (enemy.m_bDead)
-        return WORST_SCORE;
-    return 0;
+const InGame = function InGame() {
+    return GAME.m_State.m_EnemyManager && GAME.m_State.m_EnemyManager.m_bIsInteractive; 
 }
+
+const WORST_SCORE = -1 / 0;
+const START_POS = gApp.renderer.width;
+
+
+const EnemySpeed = function EnemySpeed(enemy) {
+    return enemy.m_Sprite.vx;
+}
+const EnemyDistance = function EnemyDistance(enemy) {
+    return (enemy.m_Sprite.x - k_nDamagePointx) / (START_POS - k_nDamagePointx);
+}
+
+
+class Attack {
+    process(enemies) {
+        throw new Error("not implemented");
+    }
+}
+
+// Basic clicking attack, attack closest
+class ClickAttack extends Attack {
+    score(enemy) {
+        if (enemy.m_bDead)
+            return WORST_SCORE;
+        return EnemyDistance(enemy);
+    }
+    process(enemies) {
+        let target, target_score = WORST_SCORE;
+
+        enemies.forEach(function FindBest(enemy) {
+            if (!enemy.m_Sprite.visible)
+                return;
+            let now_score = ScoreEnemyForClicking(enemy);
+            if (now_score > target_score) {
+                target = enemy, target_score = now_score;
+            }
+        });
+
+        if (target)
+            this.attack(target);
+    }
+    attack(enemy) {
+        enemy.m_Sprite.click();
+    }
+}
+
+let attacks = [
+    new ClickAttack(),
+]
 
 setInterval(function game_think() {
     let state = GAME.m_State.m_EnemyManager;
 
-    if (!state) {
-        console.log(`Not in game currently!`);
+    if (!InGame()) {
         return;
     }
-
-    if (!state.m_bIsInteractive) {
-        console.log(`Not interactive currently!`);
-        return;
-    }
-
 
     let enemies = state.m_rgEnemies;
-    let target, target_score = WORST_SCORE;
-    enemies.forEach(function FindBest(enemy) {
-        let now_score = ScoreEnemy(enemy);
-        if (now_score > target_score) {
-            target = enemy, target_score = now_score;
-        }
-    });
 
-
-    if (target) {
-        target.m_Sprite.click();
-        console.log(`Current target: ${now.toString()}" (${target_score})`);
-    } 
-    else {
-        console.log("No target");
-    }
-
+    for (let attack of attacks)
+        attack.process(enemies);
 }, 1000 / CLICKS_PER_SECOND);
