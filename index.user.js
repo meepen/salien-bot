78a91e0eb4656d82bc88bd09e4d6c57fa054b548
Meepen <meepdarknessmeep@gmail.com>
2018-06-21 19:48:16 -0400

    increase version

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Saliens bot
 // @namespace    http://tampermonkey.net/
-// @version      6
+// @version      7
 // @description  Beat all the saliens levels
 // @author       https://github.com/meepen/salien-bot
 // @match        https://steamcommunity.com/saliengame/play
