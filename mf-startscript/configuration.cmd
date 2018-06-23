c78c951fcd6281ed0d34d9493056991d371cd225
Main Fighter <MainFighter@users.noreply.github.com>
2018-06-24 07:17:47 +1000

    autodownload and autoupdate

--- mf-startscript/configuration.cmd
+++ mf-startscript/configuration.cmd
@@ -1,4 +1,17 @@
 :: Global Configuration
-:: set killrunning==true not functional
-:: set gitdownload==true not functional
+:: set killrunning=true not functional
+
+:: Automatically downloads bot files using git
+:: You need git for this to work https://git-scm.com/downloads
+set autodownloadbot=true
+
+:: Automatically updates bot files using git
+:: You need git for this to work https://git-scm.com/downloads
+set autoupdatebot=true
+
+:: Runs NPM Install everytime bot starts
 set npminstall=true
+
+:: Debug Mode
+:: You won't need to use it
+set debug==false
--- mf-startscript/win_startscript.cmd
+++ mf-startscript/win_startscript.cmd
@@ -2,7 +2,7 @@
 
 :: Made by Main Fighter [mainfighter.com]
 :: Simple start script for meepen's sailen-bot [https://github.com/meepen/salien-bot]
-:: v1.3.1 [23-06-2018]
+:: v1.4.0 [23-06-2018]
 
 ::===============================================================================================================::
 
@@ -15,8 +15,24 @@ echo Starting Sailen Bots
 
 ::===============================================================================================================::
 
+:: Debug
+if %debug%==true @echo on
+
+set rootdir=%~dp0
+
+:: Clone botfiles
+echo Downloading bot files
+for %%a in ("instances\*.cmd") do call "%%a" & call :DownloadBotFiles
+if %debug%==true pause
+
+:: Update botfiles
+echo Updating bot files
+for %%a in ("instances\*.cmd") do call "%%a" & call :UpdateBotFiles
+if %debug%==true pause
+
 :: Start all bots in config
 for %%a in ("instances\*.cmd") do call "%%a" & call :StartScript
+if %debug%==true pause
 
 ::===============================================================================================================::
 
@@ -28,18 +44,75 @@ exit
 
 ::===============================================================================================================::
 
+:DownloadBotFiles
+
+echo Downloading Bot Files for %name%
+
+cd "%rootdir%"
+
+:: Sets directory to be the same as name if not defined
+if not defined directory set directory=%name%
+
+:: Using this to skip as a workaround for issue I was having
+if %enabled%==false goto :eof
+if %autodownloadbot%==false goto :eof
+
+:: Checks if bot files don't already exist > if they don't creates folder > if they don't clones bot to directory
+if not exist botfiles\%directory% ( mkdir botfiles\%directory% & git clone https://github.com/meepen/salien-bot.git botfiles\%directory% ) else ( echo Bot files already exist )
+
+call :SetDefaults
+
+goto :eof
+
+::===============================================================================================================::
+
+:UpdateBotFiles
+
+echo Updating Bot Files for %name%
+
+cd "%rootdir%"
+
+:: Sets directory to be the same as name if not defined
+if not defined directory set directory=%name%
+
+:: Using this to skip as a workaround for issue I was having
+if %enabled%==false goto :eof
+if %autoupdatebot%==false goto :eof
+
+if exist botfiles\%directory% ( cd botfiles\%directory% & git pull ) else ( echo Bot files don't exist )
+
+call :SetDefaults
+
+goto :eof
+
+::===============================================================================================================::
+
 :StartScript
 
 echo Starting %name%
+
+cd "%rootdir%"
+
+:: Sets directory to be the same as name if not defined
+if not defined directory set directory=%name%
 
 :: Opens CMD Window > Sets title and color of window > Changes to dir > runs npm install if enabled > starts bot
 set commandline="title Sailen Bot - %name% & color %color% & cd botfiles\%directory% & if %npminstall%==true call npm install & node headless & exit"
 if %enabled%==true if %minimized%==true (start /min cmd /k  %commandline%) else (start cmd /k %commandline%)
 
+call :SetDefaults
+
+goto :eof
+
+::===============================================================================================================::
+
+:SetDefaults
+
+:: Don't change these
 set enabled=false
 set minimized=false
 set name=untitled
-set directory=notconfigured
+set directory=
 set color=0C
 
-goto eof
+goto :eof
