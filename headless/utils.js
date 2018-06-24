6abf483d416b8a407e1be1ae5eae679ce1c06f90
Gaspar Dip <gaspar.dip@hotmail.com>
2018-06-24 04:48:12 -0300

    Refactor logic using ES2017 async/await, add vscode files

---
+++ headless/utils.js
@@ -0,0 +1,115 @@
+const start_time = (Date.now() / 1000) | 0;
+
+function FormatTimer(timeInSeconds) {
+    if (parseInt(timeInSeconds) <= 0) {
+        return '';
+    }
+
+    const SECONDS_IN_MINUTE = 60;
+    const MINUTES_IN_HOUR = 60;
+    const HOURS_IN_DAY = 24;
+    const SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR;
+    const SECONDS_IN_DAY = SECONDS_IN_HOUR * HOURS_IN_DAY;
+
+    const days = Math.floor(timeInSeconds / SECONDS_IN_DAY);
+    const hours = Math.floor(timeInSeconds / SECONDS_IN_HOUR % HOURS_IN_DAY);
+    const minutes = Math.floor(timeInSeconds / SECONDS_IN_MINUTE % MINUTES_IN_HOUR);
+    const seconds = timeInSeconds % SECONDS_IN_MINUTE;
+
+    let formatted = '';
+
+    formatted += days ? `${days}d ` : '';
+    formatted += hours ? `${hours}h ` : '';
+    formatted += minutes ? `${minutes}m `: '';
+    formatted += seconds ? `${seconds}s ` : '';
+
+    return formatted;
+}
+
+function PrintInfo(cl, waitTime) {
+    // clear screen, taken from https://stackoverflow.com/a/14976765
+    let info_lines = [];
+
+    if (cl.gPlayerInfo) {
+        let info = cl.gPlayerInfo;
+        info_lines.push(["Running for", FormatTimer(((Date.now() / 1000) | 0) - start_time)]);
+        info_lines.push(["Current level", `${info.level} (${info.score} / ${info.next_level_score})`]);
+        info_lines.push(["Exp since start", info.score - cl.gPlayerInfoOriginal.score]);
+        let exp_per_hour = 60 * 60 * 2400 / (waitTime + 5);
+        info_lines.push(["Estimated exp/hr", exp_per_hour | 0]);
+
+        let date = new Date();
+        let score_bias = 0;
+
+        if (cl.gPlanets) {
+            let current = cl.gPlanets[info.active_planet];
+            if (current) {
+                info_lines.push(["Current planet", `${current.state.name} [${(current.state.capture_progress * 100).toFixed(2)}%] (id ${current.id})`]);
+                if (cl.gPlayerInfo.active_zone_position) {
+                    let zoneIdx = parseInt(cl.gPlayerInfo.active_zone_position);
+                    let zoneX = zoneIdx % k_NumMapTilesW, zoneY = (zoneIdx / k_NumMapTilesW) | 0;
+                    let zone = current.zones[zoneIdx];
+
+                    if (zone) {
+                        info_lines.push(["Current zone", `(${zoneX}, ${zoneY}) (id: ${zoneIdx}) difficulty: ${difficulty_names[zone.difficulty]}`]);
+
+                        let time_left = ((cl.endGameTime - Date.now()) / 1000) | 0;
+                        date.setTime(cl.endGameTime);
+                        score_bias = difficulty_multipliers[zone.difficulty] * 5 * SCORE_TIME;
+                        info_lines.push(["Round time left", FormatTimer(time_left)]);
+                    }
+                }
+            }
+        }
+        date.setSeconds(date.getSeconds() + (info.next_level_score - info.score - score_bias) / exp_per_hour * 60 * 60);
+        info_lines.push(["Next level up", date.toLocaleString()]);
+    }
+
+    let max_length = 0;
+
+    for (let i = 0; i < info_lines.length; i++)
+        max_length = Math.max(max_length, info_lines[i][0].length);
+
+    for (let i = 0; i < info_lines.length; i++)
+        info_lines[i] = info_lines[i].join(`: ${" ".repeat(max_length - info_lines[i][0].length)}`);
+
+    console.log("\u001b[2J\u001b[0;0H" + info_lines.join("\n"));
+}
+
+function GetBestZone(planet) {
+    let bestZone;
+    let highestDifficulty = -1;
+
+    let maxProgress = 10000;
+
+    for (let idx in planet.zones) {
+        let zone = planet.zones[idx];
+
+        if (!zone.captured) {
+            if (zone.type == 4) // boss
+                return zone;
+            if (zone.difficulty > highestDifficulty) {
+                highestDifficulty = zone.difficulty;
+                maxProgress = zone.capture_progress;
+                bestZone = zone;
+            }
+            else if (zone.difficulty < highestDifficulty)
+                continue;
+
+            if (zone.capture_progress < maxProgress) {
+                maxProgress = zone.capture_progress;
+                bestZone = zone;
+            }
+        }
+    }
+
+    return bestZone;
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+module.exports.PrintInfo = PrintInfo;
+module.exports.GetBestZone = GetBestZone;
+module.exports.sleep = sleep;
