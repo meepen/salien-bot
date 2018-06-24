fb06aaee3fc9e82782fd0426eb1b365e67504f5d
Call Me Nemo <callmenemo491@users.noreply.github.com>
2018-06-24 20:02:57 +0900

    update version

--- index.user.js
+++ index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Saliens bot
 // @namespace    http://tampermonkey.net/
-// @version      24
+// @version      25
 // @description  Beat all the saliens levels
 // @author       https://github.com/meepen/salien-bot
 // @match        https://steamcommunity.com/saliengame
