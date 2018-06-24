8d099e566c940bc10c3efced79e04c74556fc7f4
Main Fighter <MainFighter@users.noreply.github.com>
2018-06-24 12:02:49 +1000

    more explaining in exampleinstance.cmd

--- mf-startscript/instances/exampleinstance.cmd
+++ mf-startscript/instances/exampleinstance.cmd
@@ -1,21 +1,24 @@
 :: Bot Configuration Options
+:: You can leave any of them out and they will just use default settings
+:: gettoken; paste your token here
 :: enabled; whether the instance is enabled or not
 :: minimized; whether the cmd window starts minimized or not
 :: name; name of the instance
-:: directory; sets the bot directory, this can be set to %name% to use the same name as the directory
+:: directory; sets the bot directory, if not set this will just use %name% as the directory name
 :: color; colors in the cmd window
 
-:: Instance Example
-:: You can leave any of these out to use the default values
+:: Example of all Configuration Options
+:: These are also the default settings
+:: set gettoken=
 :: set enabled=false
 :: set minimized=false
-:: set name=example
-:: set directory=notconfigured
-:: set color 0A
+:: set name=untitled
+:: set directory=
+:: set color=0A
 
-:: Bot Instance
+:: Bot Instance Example
+set token={"webapi_host":"https:\/\/community.steam-api.com\/","webapi_host_secure":"https:\/\/community.steam-api.com\/","token":"473jsha28s8f78391029sh27s","steamid":"76561198230883500","persona_name":"Main Fighter","success":1}
 set enabled=true
 set minimized=false
-set name=example
-set directory=notconfigured
-set color=0A
+set name=Main Fighter
+set color 0A
